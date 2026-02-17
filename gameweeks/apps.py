9d2fa9d84d432223379102fce3a5aee6bdff4dd2
iainrt <iainrt@gmail.com>
2026-02-17 14:30:33 +0000

    setup django apps

---
+++ gameweeks/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GameweeksConfig(AppConfig):
+    name = 'gameweeks'
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from goalrush!")
-
-
-if __name__ == "__main__":
-    main()
